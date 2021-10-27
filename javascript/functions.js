739f3dedfdf9761c69d7a345de55c327ce0bdb06
amayorgad <almade87@gmail.com>
2021-10-27 18:31:11 -0500

    Actualización javascript

--- javascript/functions.js
+++ javascript/functions.js
@@ -57,7 +57,7 @@ const listarPersonas = () => {
             <td id="f${contador}c1">${objPersona.apellido}</td>
             <td id="f${contador}c2">${objPersona.email}</td>
             <td id="f${contador}c3">${objPersona.telefono}</td>
-            <td> <button>Editar</button> <button onclick="eliminar('${objPersona.email}')">Eliminar</button> </td>
+            <td> <button onclick="btnEditar('${objPersona.nombre}', '${objPersona.apellido}', '${objPersona.email}', '${objPersona.telefono}')">Editar</button> <button onclick="eliminar('${objPersona.email}')">Eliminar</button> </td>
         </tr>
         `;
     });
@@ -75,4 +75,39 @@ const limpiar = () => {
     document.getElementById("apellido").value = "";
     document.getElementById("email").value = "";
     document.getElementById("telefono").value = "";
+}
+
+const btnEditar = (nombre, apellido, email, telefono) => {
+    document.getElementById("nombre").value = nombre;
+    document.getElementById("apellido").value = apellido;
+    document.getElementById("email").value = email;
+    document.getElementById("telefono").value = telefono;
+}
+
+const btnActualizar = () => {
+    let objPersona = obtenerDatos();
+    personas.map( element => {
+        if(element.email == objPersona.email){
+            element.nombre = objPersona.nombre;
+            element.apellido = objPersona.apellido;
+            element.telefono = objPersona.telefono;
+        }
+    } );
+    listarPersonas();
+    limpiar();
+}
+
+const obtenerDatos = () => {
+    //Obtener datos de los campos de texto
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let email = document.getElementById("email").value;
+    let telefono = document.getElementById("telefono").value;
+    
+    return {
+        nombre,
+        apellido,
+        email,
+        telefono
+    }
 }
